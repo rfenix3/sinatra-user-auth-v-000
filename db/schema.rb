142cb9ff5ce98363c2e6e5eaab3e8b50de9ec253
Richard Fenix <richard.fenix@gmail.com>
2018-08-20 03:05:26 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180820030153) do
-
-  create_table "tweets", force: :cascade do |t|
-    t.text    "content"
-    t.integer "user_id"
-  end
+ActiveRecord::Schema.define(version: 20180810144338) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
